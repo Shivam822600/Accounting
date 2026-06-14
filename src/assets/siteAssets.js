5d7cb738a94724b9c2dccde97d28628b7455e2a0
shivam sharma <shivamsharm958499@gmail.com>
2026-06-14 16:52:37 +0530

    Fix local image path import in siteAssets.js to fix build error

--- src/assets/siteAssets.js
+++ src/assets/siteAssets.js
@@ -1,11 +1,9 @@
-import aboutImg from 'C:/Users/ASUS/.gemini/antigravity-ide/brain/ab6d6a8d-bc0f-4191-b1e6-5d08b2502fa0/capital_edge_about_1781430028712.png';
-
 export const siteAssets = {
   logo: 'https://rpsaccountants.com.au/wp-content/uploads/2022/04/WHITE-LOGO-PNG-6-2-3.png',
   footerMark: 'https://rpsaccountants.com.au/wp-content/uploads/2022/04/WHITE-LOGO-PNG-5.png',
   homeHero: 'https://rpsaccountants.com.au/wp-content/uploads/2025/10/Group-29_7_11zon.jpg',
   team: 'https://rpsaccountants.com.au/wp-content/uploads/2025/10/business-team-celebrating-with-raised-up-hands-_10_11zon.jpg',
-  about: aboutImg,
+  about: 'https://rpsaccountants.com.au/wp-content/uploads/2025/10/business-team-celebrating-with-raised-up-hands-_10_11zon.jpg',
   advisory: 'https://rpsaccountants.com.au/wp-content/uploads/2026/01/GST-BAS-Services-in-Australia-1024x576.jpg',
   smsf: 'https://rpsaccountants.com.au/wp-content/uploads/2026/01/Superannuation-SMSF-Services-1-1024x576.jpg',
   medical: 'https://rpsaccountants.com.au/wp-content/uploads/2026/05/graphic-111-1016x1024.png',
